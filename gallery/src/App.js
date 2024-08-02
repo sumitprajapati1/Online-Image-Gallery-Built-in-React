3ec5fd556b2294479ed7228a0f719a14f5a5bdc1
sumit <prajapatisumit009@gmail.com>
2024-08-02 17:18:03 +0530

    Creating Demo Card Template

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -4,7 +4,37 @@ import './App.css';
 function App() {
   return (
    <>
-   <div className="text-3xl font-bold underline"> Hello World  </div>
+   <div> 
+    <div className="max-w-sm rounded-md overflow-hidden shadow-lg">
+      <img src="https://images.unsplash.com/photo-1495001258031-d1b407bc1776?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwaW1hZ2UlMjBsaW5rfGVufDB8fDB8fHww" alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl-mb2"> Photo by Sumit </div>
+        <ul>
+        <li>
+          <strong> Views : 4000 </strong>
+        </li>
+        <li> 
+          <strong> Likes : 300 </strong>
+        </li>
+        <li> 
+          <strong> Downloads: 200 </strong>
+        </li>
+
+      </ul>
+      </div>
+    </div>
+    <div className="px-6 py-4">
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        Tag 1
+      </span>
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        Tag 2
+      </span>
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        Tag 3
+      </span>
+    </div>
+   </div>
    </>
   );
 }
