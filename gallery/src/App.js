3e41dcbaf06ce9ce4595141323938dc263bf4ec4
sumit <prajapatisumit009@gmail.com>
2024-08-02 20:36:46 +0530

    Adding logic of Searching Images

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [term,setTerm]=useState(' ');
 
   useEffect(()=>{
-    fetch(`https://pixabay.com/api/?key=45240813-dfca0b2c82e16fb38149b8704&q=green+pots&image_type=photo&pretty=true`)
+    fetch(`https://pixabay.com/api/?key=45240813-dfca0b2c82e16fb38149b8704&q=${term}&image_type=photo&pretty=true`)
     .then(res=>res.json())
     .then(data=>
     {
@@ -17,10 +17,10 @@ function App() {
       setIsloading(false);
     })
     .catch(err=>console.log(err))
-  },[])
+  },[term])
   return (
    <>
-   <ImageSearch/>
+   <ImageSearch searchText={(text)=>setTerm(text)}/>
 
    <div className="container mx-auto">
       {isloading? <div className="h1 text-6xl text-ceter mx-auto mt-32">Loading...</div>:<div className="md:grid md:grid-cols-3 md:gap-4">
--- gallery/src/components/ImageSearch.js
+++ gallery/src/components/ImageSearch.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-export default function ImageSearch() {
+export default function ImageSearch({searchText}) {
+    const [text,setText]=useState("");
+    const handleOnSubmit =(e)=>{
+        e.preventDefault();
+        searchText(text)
+    }
     return (
-        <div class="flex items-center justify-center my-10 mx-auto mt-5">
-        <div class="flex border border-teal-500 rounded-lg overflow-hidden">
-          <input type="text" class="mr-1 px-2 py-1 w-full text-gray-700 focus:outline-none" placeholder="Search images..." aria-label="Search for images"/>
-          <button class="flex-shrink-0 px-2 py-1 bg-teal-500 hover:bg-teal-700 border-teal-500
+        <div className="flex items-center justify-center my-10 mx-auto mt-5">
+        <form onSubmit={handleOnSubmit} className="flex border border-teal-500 rounded-lg overflow-hidden">
+          <input onChange={e=>setText(e.target.value)} type="text" className="mr-1 px-2 py-1 w-full text-gray-700 focus:outline-none" placeholder="Search images..." aria-label="Search for images"/>
+          <button className="flex-shrink-0 px-2 py-1 bg-teal-500 hover:bg-teal-700 border-teal-500
            text-white hover:border-teal-700 text-sm border-4  focus:outline-none">Search</button>
-        </div>
+        </form>
       </div>
     )
 }
